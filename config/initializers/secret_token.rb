3cf6f31f5b101f26a0f2dd9df18d6a78d21d0da2
David Chang <zeta11235813@gmail.com>
2011-09-21 19:53:10 +0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NtuScheduler::Application.config.secret_token = '15cad4ee553a6fb8632db5a589bff563a6f2bb261f17b01014f091abc6543c3773d423c451fa8a4ba4c6222f60395736a6f03b06be846dc747323b570d854c09'
