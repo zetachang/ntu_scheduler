d4b7d34305b561a6be8d8c7c42c094ad90b3994b
Raincole <raincolee@gmail.com>
2011-09-25 04:10:04 +0800

    Set Koala

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def facebook_cookies
+    @facebook_cookies ||= Koala::Facebook::OAuth.new.get_user_info_from_cookie(cookies)
+  end
 end
