6bfc1474760e058005357577a71f60aac5e62c5c
Raincole <raincolee@gmail.com>
2011-09-25 08:35:30 +0800

    Moved validate code to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,23 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActionController::RoutingError, :with => :render_404
+
   protect_from_forgery
 
-  before_filter :parse_signed_request 
+  before_filter :validate_facebook
 
-  helper_method :signed_request 
-  
-  def render_404(message = "Not Found")
+  def render_404(message = "page not found")
     logger.info "Render 404 because: #{message}"
     render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false 
   end
 
-  def signed_request 
-    @signed_request 
-  end 
+  def validate_facebook
+    # if it's running outside Facebook, render 404 page
+    raise ActionController::RoutingError, "run outside Facebook" unless params[:signed_request]
 
-  def parse_signed_request 
-    if params[:signed_request]
-      @signed_request = Koala::Facebook::OAuth.new.parse_signed_request(params[:signed_request])
-    else
-      render_404("run outside of Facebook") unless params[:signed_request]
+    @signed_request = Koala::Facebook::OAuth.new.parse_signed_request(params[:signed_request])
+    unless @signed_request["user_id"]
+      @oauth = Koala::Facebook::OAuth.new
+      render :text => "<script> top.location.href = '#{@oauth.url_for_oauth_code}'; </script>"
     end
   end
 end
