8879cc1049bb3a26eb5923774a484421a154c1be
Raincole <raincolee@gmail.com>
2011-09-25 10:55:49 +0800

    Wrote the spec to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,12 +14,12 @@ class ApplicationController < ActionController::Base
     # if it's running outside Facebook, render 404 page
     raise ActionController::RoutingError, "run outside Facebook" unless params[:signed_request]
 
-    @signed_request = Koala::Facebook::OAuth.new.parse_signed_request(params[:signed_request])
+    @oauth = Koala::Facebook::OAuth.new
+    @signed_request = @oauth.parse_signed_request(params[:signed_request])
     if @signed_request["user_id"]
       @graph = Koala::Facebook::GraphAPI.new(@signed_request["oauth_token"])
     else
       # if user hasn't authorized this app, make him do it
-      @oauth = Koala::Facebook::OAuth.new
       render :text => "<script> top.location.href = '#{@oauth.url_for_oauth_code}'; </script>"
     end
   end
