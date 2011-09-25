fe8bc4befc3394747dd464c1c63986b08b1ec8fd
Raincole <raincolee@gmail.com>
2011-09-26 00:43:39 +0800

    Removed reduntant validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
   has_one :schedule
 
-  validates_presence_of :schedule_id
   validates_presence_of :name
   validates_presence_of :student_id
   validates_presence_of :facebook_uid
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 describe User do
   subject { Factory(:user) }
   it { should have_one(:schedule) }
-  it { should validate_presence_of(:schedule_id) }
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:student_id) }
   it { should validate_presence_of(:facebook_uid) }
