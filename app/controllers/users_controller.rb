fd91404b7c6ef01e3ed189f30eb15ffddf224227
Raincole <raincolee@gmail.com>
2011-10-08 11:06:05 +0800

    Changed a variable name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,16 +2,16 @@
 
 class UsersController < ApplicationController
   def create
-    respond = { :status => "ERROR" }
+    echo = { :status => "ERROR" }
 
     @user = User.new(params[:user])
     @user.update_attributes(:name => @me["name"], :facebook_uid => @me["id"])
     @user.student_id.upcase!
 
     if @user.student_id.blank?
-      respond[:message] = "請填入學號"
+      echo[:message] = "請填入學號"
     elsif @user.student_id !~ /^\w\d{8}$/
-      respond[:message] = "請檢查學號是否正確"
+      echo[:message] = "請檢查學號是否正確"
     else
 
       @schedule = Schedule.new()
@@ -20,20 +20,20 @@ class UsersController < ApplicationController
         @schedule.load_from_eportfolio(@user.student_id)
       rescue ScheduleCrawler::NoPublicError
         # TODO: add a step-by-step tutorial page
-        respond[:message] = "請將你的e-portfolio設為公開！"
+        echo[:message] = "請將你的e-portfolio設為公開！"
       rescue ScheduleCrawler::NoLessonError
-        respond[:message] = "本學期的課表尚未公佈(或計算機中心正在維修)"
+        echo[:message] = "本學期的課表尚未公佈(或計算機中心正在維修)"
       rescue ScheduleCrawler::HTTPError
-        respond[:message] = "請檢查學號是否正確"
+        echo[:message] = "請檢查學號是否正確"
       else
         @user.save!
-        respond[:status] = "SUCCESS"
+        echo[:status] = "SUCCESS"
       end
 
     end
 
     respond_to do |format|
-      format.json { render :json => respond.to_json }
+      format.json { render :json => echo.to_json }
     end
   end
 end
