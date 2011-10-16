da0b7fe581835c308054c3c891514f013e9ca8dc
Raincole <raincolee@gmail.com>
2011-10-16 18:29:36 +0800

    Removed some useless validations

--- spec/models/schedule_spec.rb
+++ spec/models/schedule_spec.rb
@@ -1,9 +1,6 @@
 require 'spec_helper'
 
 describe Schedule do
-  subject { Factory(:schedule) }
   it { should have_many(:days) }
   it { should belong_to(:user) }
-  it { should validate_presence_of(:user_id) }
-  its(:'days.size') { should == 6 }
 end
