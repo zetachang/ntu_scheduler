5175cb3add65a4d638d92d1805bab260ab0dffba
Raincole <raincolee@gmail.com>
2011-10-26 22:39:24 +0800

    Updated a spec

--- spec/models/schedule_set_spec.rb
+++ spec/models/schedule_set_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe ScheduleSet do
   subject { Factory(:schedule_set) }
-  it { should have_many(:schedule) }
+  it { should have_many(:schedules).through(:settings) }
   it { should validate_uniqueness_of(:permalink) }
   its(:permalink) {should_not be_nil}
 end
