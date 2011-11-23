ba7d3c1fc83e1ac8c60c5b0890bb06002512df3e
David Chang <zeta11235813@gmail.com>
2011-11-24 04:42:16 +0800

    Fix some weird behavior.

--- app/controllers/schedule_sets_controller.rb
+++ app/controllers/schedule_sets_controller.rb
@@ -1,7 +1,12 @@
 #encoding: UTF-8
 class ScheduleSetsController < ApplicationController
+  def index
+    @schedule_sets = current_user.schedule_sets
+    render :partial => 'schedule_sets/index', :locals => {:sets => @schedule_sets}
+  end
+
   def create_empty
-    schedule_set = ScheduleSet.new(:name => params[:name], :user_id => current_user)
+    schedule_set = ScheduleSet.new(:name => params[:name], :user_id => current_user.id)
     if schedule_set.save
       render :json => schedule_set
     else
@@ -56,13 +61,13 @@ class ScheduleSetsController < ApplicationController
     echo = { :status => "ERROR" }
 
     @schedule_set = current_user.schedule_sets.includes(:schedules => [:user, {:days => :lessons}])
-                                              .find(:first, params[:id])
+                                              .find(params[:id])
     if @schedule_set.nil?
       echo[:message] << t("schedule_sets.no_schedule_set") 
     else
       echo[:status] = "SUCCESS"
     end
-    render :partial => "schedule_sets/show", :locals => {:schedule_sets => @schedule_sets}
+    render :partial => "schedule_sets/show", :locals => {:schedule_set => @schedule_set}
   end
 
   def add_schedule
@@ -91,11 +96,10 @@ class ScheduleSetsController < ApplicationController
 
   def add_schedule_to
     echo = { :status => "ERROR" }
-
-    @schedule_set = current_user.schedule_sets.find(:first, params[:schedule_set][:id])
-    @schedule = Schedule.find(:first, params[:schedule_id])
-    echo.merge! add_schedule_to_specify_schedule_set(@schedule_set, @schedule)
-
+    @schedule_set = current_user.schedule_sets.find(params[:schedule_set][:id])
+    @schedule = Schedule.find(params[:schedule_id])
+    echo.merge! add_schedule_to_specific_schedule_set(@schedule_set, @schedule)
+    
     respond_to do |format|
       format.json { render :json => echo }
     end
@@ -103,10 +107,9 @@ class ScheduleSetsController < ApplicationController
 
   def remove_schedule_from 
     echo = { :status => "ERROR" }
-
-    @schedule_set = current_user.schedule_sets.find(:first, params[:schedule_set][:id])
-    @schedule = Schedule.find(:first, params[:schedule_id])
-    echo.merge! remove_schedule_from_specify_schedule_set(@schedule_set, @schedule)
+    @schedule_set = current_user.schedule_sets.find(params[:schedule_set][:id])
+    @schedule = Schedule.find(params[:schedule_id])
+    echo.merge! remove_schedule_from_specific_schedule_set(@schedule_set, @schedule)
 
     respond_to do |format|
       format.json { render :json => echo }
@@ -114,20 +117,24 @@ class ScheduleSetsController < ApplicationController
   end
 
   private
-  def add_schedule_to_specify_schedule_set(schedule_set, schedule)
+  def add_schedule_to_specific_schedule_set(schedule_set, schedule)
     echo = {}
+    #raise schedule.to_yaml
     if schedule_set.nil?
-      echo[:message] << t("schedule_sets.no_schedule_set") 
+      echo[:message] = t("schedule_sets.no_schedule_set") 
     elsif schedule.nil?
-      echo[:message] << t("schedules.no_schedule")
+      echo[:message] = t("schedules.no_schedule")
+    elsif schedule_set.schedules.include?(schedule)
+      echo[:message] = "Already Added"
     else
       schedule_set.schedules << schedule
       echo[:status] = "SUCCESS"
+      echo[:message] = "Added"
     end
     return echo
   end
 
-  def remove_schedule_from_specify_schedule_set(schedule_set, schedule)
+  def remove_schedule_from_specific_schedule_set(schedule_set, schedule)
     echo = {}
     if schedule_set.nil?
       echo[:message] << t("schedule_sets.no_schedule_set") 
