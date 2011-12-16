f323fe5946238235b405c12f80e9622e7853b118
Raincole <raincolee@gmail.com>
2011-12-16 15:55:02 +0800

    Clear schedule sets controller

--- app/controllers/schedule_sets_controller.rb
+++ app/controllers/schedule_sets_controller.rb
@@ -4,124 +4,38 @@ class ScheduleSetsController < ApplicationController
     @schedule_sets = current_user.schedule_sets
     render :partial => 'schedule_sets/index', :locals => {:sets => @schedule_sets}
   end
-
-  def create_empty
-    schedule_set = ScheduleSet.new(:name => params[:name], :user_id => current_user.id)
-    if schedule_set.save
-      render :json => { :status => "SUCCESS", :id => schedule_set.id, :name => schedule_set.name }
-    else
-      render :json => { :status => "INVALID", :errors => schedule_set.errors.full_messages }
-    end
-  end
   
-  def create
-    #TODO: implement
+  def create_when_compare
+    @a = Schedule.find(params[:schedule_a])
+    @b = Schedule.find(params[:schedule_b])
+    @existed_sets = ScheduleSets.joins(:settings).where(:settings => {:schedule_id=>@a})
+                                .joins(:settings).where(:settings => {:schedule_id=>@b})
+    if @existed_sets.size == 0
+      @schedule_set = ScheduleSets.create(:name => "Anonymous Set", :user_id => current_user)
+      @schedule_set.schedules << a << b;
+    else
+      @schedule_set = @existed_sets.first
+    
+    respond_to do |format|
+      format.json { render :json => {:status => "SUCCESS", :schedule_set => @schedule.id} }
+    end
   end
 
   def destroy 
-    echo = { :status => "ERROR" }
+    @schedule_set = current_user.schedule_sets.find(params[:id])
+    @schedule_set.destroy
 
-    @schedule_set = current_user.schedule_sets.find(:first, params[:id])
-    if @schedule_set.nil?
-      echo[:message] << t("schedule_sets.no_schedule_set") 
-    else
-      @schedule_set.destroy
-      echo[:status] = "SUCCESS"
-    end
-      
     respond_to do |format|
-      format.json { render :json => echo }
+      format.json { render :json => {:status => "SUCCESS"} }
     end
   end
 
   def show
-    echo = { :status => "ERROR" }
-
     @schedule_set = current_user.schedule_sets.includes(:schedules => [:user, {:days => :lessons}])
                                               .find(params[:id])
-    if @schedule_set.nil?
-      echo[:message] << t("schedule_sets.no_schedule_set") 
-    else
-      echo[:status] = "SUCCESS"
-    end
-    render :partial => "schedule_sets/show", :locals => {:schedule_set => @schedule_set}
-  end
-
-  def add_schedule
-    echo = { :status => "ERROR" }
-
-    @schedule_set = current_user.schedule_sets.find(:first, params[:id])
-    @schedule = current_user.schedules.find(:first, params[:schedule_id])
-    echo.merge! add_schedules_to_specify_schedule_set(@schedule_set, @schedule)
-
     respond_to do |format|
-      format.json { render :json => echo }
+      format.json { render :partial => "schedule_sets/show", :locals => {:schedule_set => @schedule_set} }
     end
-  end
-
-  def remove_schedule
-    echo = { :status => "ERROR" }
-
-    @schedule_set = current_user.schedule_sets.find(:first, params[:id])
-    @schedule = @schedule_set.schedules.find(:first, params[:schedule])
-    echo.merge! remove_schedule_from_specify_schedule_set(@schedule_set, @schedule)
-      
-    respond_to do |format|
-      format.json { render :json => echo }
-    end
-  end
-
-  def add_schedule_to
-    echo = { :status => "ERROR" }
-    @schedule_set = current_user.schedule_sets.find(params[:schedule_set][:id])
-    @schedule = Schedule.find(params[:schedule_id])
-    echo.merge! add_schedule_to_specific_schedule_set(@schedule_set, @schedule)
-    
-    respond_to do |format|
-      format.json { render :json => echo }
-    end
-  end
-
-  def remove_schedule_from 
-    echo = { :status => "ERROR" }
-    @schedule_set = current_user.schedule_sets.find(params[:schedule_set][:id])
-    @schedule = Schedule.find(params[:schedule_id])
-    echo.merge! remove_schedule_from_specific_schedule_set(@schedule_set, @schedule)
-
-    respond_to do |format|
-      format.json { render :json => echo }
-    end
-  end
-
-  private
-  def add_schedule_to_specific_schedule_set(schedule_set, schedule)
-    echo = {}
-    #raise schedule.to_yaml
-    if schedule_set.nil?
-      echo[:message] = t("schedule_sets.no_schedule_set") 
-    elsif schedule.nil?
-      echo[:message] = t("schedules.no_schedule")
-    elsif schedule_set.schedules.include?(schedule)
-      echo[:message] = "Already Added"
-    else
-      schedule_set.schedules << schedule
-      echo[:status] = "SUCCESS"
-      echo[:message] = "Added"
-    end
-    return echo
-  end
-
-  def remove_schedule_from_specific_schedule_set(schedule_set, schedule)
-    echo = {}
-    if schedule_set.nil?
-      echo[:message] << t("schedule_sets.no_schedule_set") 
-    elsif schedule.nil?
-      echo[:message] << t("schedules.no_schedule")
-    else
-      schedule_set.schedules.delete(schedule)
-      echo[:status] = "SUCCESS"
-    end
-    return echo
   end
 
 end
