1e3abbc9c38aead76a8d4e46d594d14d71926a71
David Chang <zeta11235813@gmail.com>
2011-12-26 20:47:48 +0800

    Add asset host for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,5 +56,7 @@ NtuScheduler::Application.configure do
   config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners
-  config.active_support.deprecation = :notify  
+  config.active_support.deprecation = :notify
+  
+  ActionController::Base.asset_host = "http://everyscheduler.herokuapp.com/"  
 end
