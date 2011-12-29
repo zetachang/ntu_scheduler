749fd7598c17e24b8e59b7c4b83a25aaac2c71af
Raincole <raincolee@gmail.com>
2011-12-29 10:36:23 +0800

    Fixed the time number

--- app/helpers/schedules_helper.rb
+++ app/helpers/schedules_helper.rb
@@ -1,2 +1,14 @@
 module SchedulesHelper
+  def time_number(time)
+    case time
+    when 0..4
+      time
+    when 5
+      '@'
+    when 6..10
+      time-1
+    when 11..14
+      (time - 11 + 'A'.ord).chr
+    end
+  end
 end
